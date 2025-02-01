a365a77dbf5a74ad7ae2cbfcad0bf0d22607ad72
SimonMartineau <martineausimon20@gmail.com>
2025-02-01 16:50:16 +0000

    Completed backend functions

--- Classes/functions.php
+++ Classes/functions.php
@@ -96,162 +96,141 @@ function update_backend_data(){
     update_volunteer_data();
     update_check_data();
     update_purchase_data();
+    update_junction_data();
     update_activities_data();
 }
 
-// This function updates the points, hours_required and hours_completed for Volunteers table.
+
+// This function updates the backend data of Volunteers (points, hours_required, hours_completed).
 function update_volunteer_data(){
-
-    // Fetching all volunteer data
-    $all_volunteer_data = fetch_data("select * from Volunteers");
-
     // Initialise Database object
     $DB = new Database();
 
-    // Navigating each volunteer to update one by one
-    foreach($all_volunteer_data as $volunteer_data){
+    // SQL query to update points, hours_required for Volunteers
+    $points_hours_required_query = 
+           "UPDATE Volunteers v
+            LEFT JOIN Checks c ON v.id = c.volunteer_id AND c.check_active = 1
+            SET v.points = COALESCE(c.points_deposit - c.points_spent, 0),
+                v.hours_required = COALESCE(c.hours_required, 0);";
 
-        // Current volunteer id
-        $volunteer_id = $volunteer_data['id'];
+    $DB->update($points_hours_required_query);
 
-        // Looking if the volunteer has a check in effect
-        $current_check_query = "SELECT * FROM Checks WHERE CURRENT_DATE BETWEEN issuance_date AND validity_date AND volunteer_id = '$volunteer_id'";
-        $current_check_data = fetch_data($current_check_query);
+    // SQL query to update hours_completed for Volunteers
+    $hours_completed_query = 
+           "UPDATE Volunteers v
+            LEFT JOIN Checks c ON v.id = c.volunteer_id AND c.check_active = 1
+            SET v.hours_completed = COALESCE(c.hours_completed, 0);";
 
-        // If there is no check in effect, $current_check_data = false and we skip updating the points.
-        if ($current_check_data && !empty($current_check_data)) {
-            // Access the first row
-            $current_check_row = $current_check_data[0];
-
-            // Getting the points_deposit from the check.
-            $points = $current_check_row['points_deposit'];
-            $hours_required = $current_check_row['hours_required'];
-
-            $points -= $current_check_row['points_spent'];
-
-        } else{
-                // The volunteer has no check so no points;
-                $points = 0;
-                $hours_required = 0;
-        }
-
-        // SQL query into Volunteers
-        $volunteers_query = "UPDATE Volunteers 
-            SET points = '$points',
-                hours_required = '$hours_required'
-            WHERE id = '$volunteer_id';";
-
-        $DB->update($volunteers_query);
-    }
+    $DB->update($hours_completed_query);
 }
 
-// This function updates the hours_required and hours_completed for Checks table.
+
+// This function updates the backend data of Checks (hours_required, hours_completed, check_active).
 function update_check_data(){
-
-    // Fetching all volunteer data
-    $all_checks_data = fetch_data("select * from Checks");
-
     // Initialise Database object
     $DB = new Database();
 
-    // Navigating each volunteer to update one by one
-    foreach($all_checks_data as $check_data){
+    // SQL query to update points_spent for Checks
+    $points_spent_query =
+    "UPDATE Checks
+            SET points_spent = (
+                SELECT IFNULL(SUM(total_cost), 0)
+                FROM Purchases
+                WHERE Purchases.check_id = Checks.id
+            );";
 
-        // Current volunteer id
-        $check_id = $check_data['id'];
+    $DB->update($points_spent_query);
 
-        // Getting the issuance_date and validity_date from the current check
-        $issuance_date = $check_data['issuance_date'];
-        $validity_date = $check_data['validity_date'];
+    // SQL query to update hours_completed for Checks
+    $hours_completed_query = 
+        "UPDATE Checks c
+                SET hours_completed = (
+                    SELECT IFNULL(SUM(a.activity_duration), 0)
+                    FROM Activities a
+                    JOIN Volunteer_Activity_Junction vaj ON vaj.activity_id = a.id
+                    WHERE vaj.check_id = c.id
+                );";
+    
+    $DB->update($hours_completed_query);
 
-        $points_spent = fetch_data("
-            SELECT SUM(total_cost) 
-            FROM Purchases 
-            WHERE check_id='$check_id'
-            AND purchase_date BETWEEN '$issuance_date' AND '$validity_date'"
-        )[0]['SUM(total_cost)'] ?? 0;
+    // SQL query to update check_active for Checks
+    $check_active_query = 
+       "UPDATE Checks c
+        SET c.check_active = CASE
+                                WHEN CURRENT_DATE BETWEEN c.issuance_date AND c.validity_date
+                                    AND c.issuance_date = (
+                                        SELECT MAX(issuance_date)
+                                        FROM Checks
+                                        WHERE volunteer_id = c.volunteer_id
+                                        AND CURRENT_DATE BETWEEN issuance_date AND validity_date
+                                    ) THEN 1
+                                ELSE 0
+                            END;";
 
-        // Placeholder for hours_completed
-        $hours_completed = 0;
-
-        $check_active = fetch_data("
-            SELECT 
-                CASE 
-                    WHEN CURRENT_DATE BETWEEN '$issuance_date' AND '$validity_date' THEN 1
-                    ELSE 0
-                END AS check_active
-            FROM Checks
-            WHERE id = '$check_id'
-        ")[0]['check_active'] ?? 0;
-
-        // SQL query into Volunteers
-        $volunteers_query = "UPDATE Checks 
-            SET points_spent = '$points_spent',
-                hours_completed = '$hours_completed',
-                check_active = '$check_active'
-            WHERE id = '$check_id';";
-
-        $DB->update($volunteers_query);
-    }
+    $DB->update($check_active_query);
 }
 
-// This function updates the hours_required and hours_completed for Checks table.
-// This function updates the backend data of purchases.
+
+// This function updates the backend data of Purchases (check_id).
 function update_purchase_data(){
-
-    // Fetching all volunteer data
-    $all_purchases_data = fetch_data("select * from Purchases");
-
     // Initialise Database object
     $DB = new Database();
 
-    // Navigating each volunteer to update one by one
-    foreach($all_purchases_data as $purchase_data){
+    // SQL query to update check_id for Purchases, defaults to -1 if no corresponding check
+    $purchase_check_id_query =
+       "UPDATE Purchases p
+        SET p.check_id = COALESCE((
+            SELECT c.id
+            FROM Checks c
+            WHERE c.volunteer_id = p.volunteer_id
+            AND p.purchase_date BETWEEN c.issuance_date AND c.validity_date
+            LIMIT 1
+        ),-1);";
 
-        $purchase_id = $purchase_data['id'];
-        $purchase_date = $purchase_data['purchase_date'];
-        $volunteer_id = $purchase_data['volunteer_id'];
-
-        $result = fetch_data("
-            SELECT id 
-            FROM Checks
-            WHERE '$purchase_date' BETWEEN issuance_date AND validity_date
-            AND '$volunteer_id' = volunteer_id");
-
-        // Get the first matching check ID or -1 if none found
-        $check_id = $result[0]['id'] ?? -1;
-
-        // SQL query into Purchases
-        $purchases_query = "UPDATE Purchases 
-            SET check_id = '$check_id'
-            WHERE id = '$purchase_id';";
-
-        $DB->update($purchases_query);
-    }
+    $DB->update($purchase_check_id_query);
+    
 }
 
-// This function updates the backend data of activities.
-function update_activities_data(){    
 
+// This function updates the backend data of Volunteer_Activity_Junction (check_id).
+function update_junction_data(){    
     // Initialise Database object
     $DB = new Database();
 
     // This SQL query associated every assigned activity to a volunteer's check if the purchase date matches. 
     // If not, default value (-1) is given to check_id in Volunteer_Activity_Junction
-    $update_volunteer_activity_junction_data = "UPDATE Volunteer_Activity_Junction vaj
-                        JOIN Activities a ON vaj.activity_id = a.id
-                        SET vaj.check_id = COALESCE((
-                            SELECT c.id
-                            FROM Checks c
-                            WHERE c.volunteer_id = vaj.volunteer_id
-                            AND a.activity_date BETWEEN c.issuance_date AND c.validity_date
-                            LIMIT 1
-                        ), -1);
-    ";
+    $check_id_query = 
+           "UPDATE Volunteer_Activity_Junction vaj
+            JOIN Activities a ON vaj.activity_id = a.id
+            SET vaj.check_id = COALESCE((
+                SELECT c.id
+                FROM Checks c
+                WHERE c.volunteer_id = vaj.volunteer_id
+                AND a.activity_date BETWEEN c.issuance_date AND c.validity_date
+                LIMIT 1
+            ), -1);";
 
     // Update the database
-    $DB->update($update_volunteer_activity_junction_data);
+    $DB->update(query: $check_id_query);
+}
 
+
+// This function updates the backend data of Activities (number_of_participants).
+function update_activities_data(){ 
+    // Initialise Database object
+    $DB = new Database();
+    
+    // SQL query to update number_of_participants for Activities
+    $number_of_participants_query = 
+           "UPDATE Activities a 
+            SET a.number_of_participants = (
+                SELECT COUNT(*) 
+                FROM Volunteer_Activity_Junction vaj
+                WHERE vaj.activity_id = a.id
+            );";
+
+    // Update the database
+    $DB->update(query: $number_of_participants_query);
 }
 
 
